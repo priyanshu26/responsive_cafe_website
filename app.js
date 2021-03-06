62d31c6057a57021883070021bd28b2eab1a16b7
priyanshu26 <79014731+priyanshu26@users.noreply.github.com>
2021-03-06 08:46:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function slideShow() {
+    let navBar = document.getElementById('check-class');
+    if(navBar.style.display === "none"){
+        navBar.style.display = "block";
+    }
+    else{
+        navBar.style.display = "none";
+    }
+}
